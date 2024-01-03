81aa24bab84072b9d319f4d535f50d96bb670791
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2024-01-03 11:48:47 -0500

    Buscador de noticias

---
+++ buscadornoticias/src/components/Noticia.jsx
@@ -0,0 +1,55 @@
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+
+const Noticia = ({noticia}) => {
+
+    const { urlToImage, url, title, description, source } = noticia
+    return (
+        <Grid item md={6} lg={4} sx={{ display: 'flex' }}>
+        <Card sx={{ flex: 1 }}>
+          {urlToImage && (
+            <CardMedia
+              component="img"
+              alt={`Imagen de la noticia ${title}`}
+              image={urlToImage}
+              height={'250'}
+            />
+          )}
+  
+          <CardContent>
+            <Typography variant='body1' color="error">
+              {source.name}
+            </Typography>
+            <Typography variant='h5' component="div">
+              {title}
+            </Typography>
+            <Typography variant='body2'>
+              {description}
+            </Typography>
+          </CardContent>
+  
+          <CardActions>
+            <Link
+              href={url}
+              target="_blank"
+              variant='button'
+              width={'100%'}
+              textAlign={'center'}
+              sx={{
+                textDecoration: 'none',
+              }}
+            >
+              Leer Noticia
+            </Link>
+          </CardActions>
+        </Card>
+      </Grid>
+    )
+}
+
+export default Noticia
